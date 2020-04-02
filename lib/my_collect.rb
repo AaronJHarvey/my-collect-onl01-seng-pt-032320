c02efa94e47b88657037b2a1988284060b25cf25
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-02 19:27:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
  modified_collection=[]
  counter=1
  while counter < collection.length
- modified_collection << yield(collection[counter])
+ modified_collection << yield(collection.upcase)
    counter +=1 
  end
 modified_collection
