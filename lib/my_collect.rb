e0c75702d60cc9b89e4df957d66b1ebafc9147c8
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-02 19:28:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+ modified_collection=[]
+ counter=0
+ while counter < collection.length
+ modified_collection << yield(collection[counter])
+   counter +=1 
+ end
+modified_collection
+end
